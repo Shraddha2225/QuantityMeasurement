4f0142b943a846a5c8fbd53f2958b22732057e30
Shraddha2225 <somanip09@gmail.com>
2020-03-16 11:17:16 +0530

    ADDED:Code to check equality based on inch

--- src/main/java/com/measurement/QuantityMeasurementForFeet.java
+++ src/main/java/com/measurement/QuantityMeasurementForFeet.java
@@ -1,22 +1,23 @@
 package com.measurement;
 import java.util.Objects;
 
-public class QuantityMeasurement<E>{
-    private int inch;
+public class QuantityMeasurementForFeet<E>{
     private int feet;
-    public QuantityMeasurement() {
+    private int inch;
+    public QuantityMeasurementForFeet() {
         this.feet = feet;
-        this.inch = inch;
+
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        QuantityMeasurement that = (QuantityMeasurement) o;
-        return inch == that.inch &&
-                feet == that.feet;
+        QuantityMeasurementForFeet<?> that = (QuantityMeasurementForFeet<?>) o;
+        return feet == that.feet;
     }
+
+
 
     public boolean valueCheck(E value1, E value2) {
         return(Objects.equals(value1, value2));
@@ -26,4 +27,5 @@ public class QuantityMeasurement<E>{
         inch = feetValue * 12;
         return inch;
     }
+
 }
--- src/test/java/com/measurement/TestQuantityMeasurement.java
+++ src/test/java/com/measurement/TestQuantityMeasurement.java
@@ -5,28 +5,33 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestQuantityMeasurement {
-    private QuantityMeasurement quantityMeasurement;
+    private QuantityMeasurementForFeet quantityMeasurement;
+    private QuantityMeasurementForInch quantityMeasurementForInch;
 
     @Before
     public void setUp() {
-        quantityMeasurement = new QuantityMeasurement();
+        quantityMeasurement = new QuantityMeasurementForFeet();
+        quantityMeasurementForInch = new QuantityMeasurementForInch();
     }
 
     @Test
     public void givenEqualValue_ShouldReturnTrue() {
-        boolean result = quantityMeasurement.valueCheck(0,0);
+        int result1 = quantityMeasurement.conversionFeetToInch(0);
+        int result2 = quantityMeasurement.conversionFeetToInch(0);
+        boolean result = quantityMeasurement.valueCheck(result1, result2);
         Assert.assertEquals(true, result);
     }
+
     @Test
     public void givenNull_ShouldReturnFalse() {
-        int FEET=quantityMeasurement.conversionFeetToInch(0);
-        boolean result = quantityMeasurement.valueCheck(null,FEET);
+        int FEET = quantityMeasurement.conversionFeetToInch(0);
+        boolean result = quantityMeasurement.valueCheck(null, FEET);
         Assert.assertEquals(false, result);
     }
 
     @Test
     public void givenReference_ShouldReturnTrue() {
-        QuantityMeasurement referenceTwo = new QuantityMeasurement();
+        QuantityMeasurementForFeet referenceTwo = new QuantityMeasurementForFeet();
         referenceTwo.conversionFeetToInch(2);
         quantityMeasurement.conversionFeetToInch(2);
         Assert.assertEquals(quantityMeasurement, referenceTwo);
@@ -40,13 +45,282 @@ public class TestQuantityMeasurement {
 
     @Test
     public void givenFeetValue_WhenEqual_ShouldReturnTrue() {
-        boolean result = quantityMeasurement.valueCheck(2, 2);
-        Assert.assertTrue(result);
+        int result1 = quantityMeasurement.conversionFeetToInch(2);
+        int result2 = quantityMeasurement.conversionFeetToInch(2);
+        boolean result = quantityMeasurement.valueCheck(result1, result2);
+        Assert.assertEquals(true, result);
     }
 
     @Test
     public void givenFeetValue_WhenNotEqual_ShouldReturnFalse() {
-        boolean result = quantityMeasurement.valueCheck(2, 1);
-        Assert.assertFalse(result);
+        int result1 = quantityMeasurement.conversionFeetToInch(2);
+        int result2 = quantityMeasurement.conversionFeetToInch(1);
+        boolean result = quantityMeasurement.valueCheck(result1, result2);
+        Assert.assertEquals(false, result);
+    }
+
+    @Test
+    public void givenEqualValueForInch_ShouldReturnTrue() {
+        double result1 = quantityMeasurementForInch.conversionInchToFeet(0);
+        double result2 = quantityMeasurementForInch.conversionInchToFeet(0);
+        boolean result = quantityMeasurementForInch.valueCheckForInch(result1, result2);
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenNullForInch_ShouldReturnFalse() {
+        double Inch = quantityMeasurementForInch.conversionInchToFeet(0);
+        boolean result = quantityMeasurement.valueCheck(null, Inch);
+        Assert.assertEquals(false, result);
+    }
+
+    @Test
+    public void givenReferenceForInch_ShouldReturnTrue() {
+        QuantityMeasurementForInch referenceTwo = new QuantityMeasurementForInch();
+        referenceTwo.conversionInchToFeet(2);
+        quantityMeasurementForInch.conversionInchToFeet(2);
+        Assert.assertEquals(quantityMeasurementForInch, referenceTwo);
+    }
+
+
+    @Test
+    public void givenTypeForInch_ShouldReturnTrue() {
+        boolean result = quantityMeasurementForInch.getClass().equals(this.getClass());
+        Assert.assertEquals(false, result);
+    }
+
+    @Test
+    public void givenValueCheckForInch_WhenEqual_ShouldReturnTrue() {
+        double result1 = quantityMeasurementForInch.conversionInchToFeet(3);
+        double result2 = quantityMeasurementForInch.conversionInchToFeet(3);
+        boolean result = quantityMeasurementForInch.valueCheckForInch(result1, result2);
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenValueCheckForInch_WhenNotEqual_ShouldReturnFalse() {
+        double result1 = quantityMeasurementForInch.conversionInchToFeet(12);
+        double result2 = quantityMeasurementForInch.conversionInchToFeet(4);
+        boolean result = quantityMeasurementForInch.valueCheckForInch(result1, result2);
+        Assert.assertEquals(false, result);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
